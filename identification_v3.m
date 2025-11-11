e0b59b1f43327bcaf7a75c8d1b781d1b22e7afe5
c-radicallis <afonso.henrique@live.com.pt>
2025-11-11 10:48:24 +0000

    commit

--- identification_v3.m
+++ identification_v3.m
@@ -72,7 +72,7 @@ G_PIDF_true_tune_data11 = feedback(true_tune*fpga_n4sid_data11_openloop, 1);
 %% Data 12
 folder_0711 ='C:\Users\afons\OneDrive - Universidade de Lisboa\Controlo de Plataforma Sismica\minimesa_data\7-11-2025\';
 file = 'pink_noise_40Hz_T3mm_0_P7.acq'; % load output acq
-true_tune = pid(5,0,0,0.0019455 , Ts_fpga  );
+true_tune = pid(7,0,0,0.0019455 , Ts_fpga  );
 %scale = 1;
 LTF_to_TXT_then_load_wSV( file , folder_0711 , 'OutputFolder', folder_0711);
 
@@ -113,7 +113,7 @@ G_PIDF_true_tune_data12 = feedback(true_tune*fpga_n4sid_data12_openloop, 1);
 %% Data 13
 folder_0711 ='C:\Users\afons\OneDrive - Universidade de Lisboa\Controlo de Plataforma Sismica\minimesa_data\7-11-2025\';
 file = 'pink_noise_40Hz_T3mm_0_P10.acq'; % load output acq
-true_tune = pid(5,0,0,0.0019455 , Ts_fpga  );
+true_tune = pid(10,0,0,0.0019455 , Ts_fpga  );
 %scale = 1;
 LTF_to_TXT_then_load_wSV( file , folder_0711 , 'OutputFolder', folder_0711);
 
@@ -151,6 +151,46 @@ tuner_opts = pidtuneOptions('DesignFocus','reference-tracking');
 
 G_PIDF_true_tune_data13 = feedback(true_tune*fpga_n4sid_data13_openloop, 1);
 
+%% Data 14
+folder_0711 ='C:\Users\afons\OneDrive - Universidade de Lisboa\Controlo de Plataforma Sismica\minimesa_data\7-11-2025\';
+file = 'pink_noise_40Hz_T3mm_0_P15.acq'; % load output acq
+true_tune = pid(15,0,0,0.0019455 , Ts_fpga  );
+%scale = 1;
+LTF_to_TXT_then_load_wSV( file , folder_0711 , 'OutputFolder', folder_0711);
+
+x_acq_T = x_acq_T*1e3;
+sv2_acq = -sv2_acq; %output is inverted because the wiring is fliped
+
+% [c_drv,lags_drv] = xcorr(x_drv_T_0,x_acq_T);
+% [c_sv,lags_sv] = xcorr(sv2_acq,x_acq_T,'normalized');
+% figure(91); stem(lags_drv,c_drv); figure(92); stem(lags_sv,c_sv);
+
+data14_openloop = iddata(x_acq_T, sv2_acq, Ts);data14_openloop.InputName  = 'sv2_acq';data14_openloop.OutputName = 'x_acq_T';data14_openloop.TimeUnit   = 'seconds';
+
+n1 = numel(x_drv_T_0);n2 = numel(x_acq_T);nmin = min(n1, n2);
+data14_closedloop =  iddata(x_acq_T(1:nmin), x_drv_T_0(1:nmin), Ts);data14_closedloop.InputName  = 'x_drv_T_0';data14_closedloop.OutputName = 'x_acq_T';data14_closedloop.TimeUnit   = 'seconds';
+
+% Open Loop
+g_data14_openloop = spa(data14_openloop, 30);
+
+% Model training
+nx = 6 ;
+n4sid_data14_openloop = n4sid(data14_openloop,nx,'Ts',Ts,n4sidOpt); n4sid_data14_openloop.InputName  = data14_openloop.InputName;n4sid_data14_openloop.OutputName = data14_openloop.OutputName;
+
+% Resampled to 5000 Hz
+fpga_n4sid_data14_openloop = d2d(n4sid_data14_openloop, Ts_fpga);
+
+% PIDF tuning
+tuner_opts = pidtuneOptions('DesignFocus','reference-tracking');
+ 
+% cutoff_frequency = 5; % Hz
+% PIDF  = pidtune(fpga_n4sid_data14_openloop,'PIDF',cutoff_frequency*2*pi,tuner_opts)
+% G_PIDF_5Hz = feedback(PIDF*fpga_n4sid_data14_openloop, 1);
+
+% PIDF  = pidtune(fpga_n4sid_data14_openloop,'PIDF',tuner_opts)
+% G_PIDF_tracking_data14 = feedback(PIDF*fpga_n4sid_data14_openloop, 1);
+
+G_PIDF_true_tune_data14 = feedback(true_tune*fpga_n4sid_data14_openloop, 1);
 
 %% Data Laquila
 % % input file
@@ -174,20 +214,14 @@ G_PIDF_true_tune_data13 = feedback(true_tune*fpga_n4sid_data13_openloop, 1);
 g_data11_closedloop = spa(data11_closedloop, 30);
 g_data12_closedloop = spa(data12_closedloop, 30);
 g_data13_closedloop = spa(data13_closedloop, 30);
+g_data14_closedloop = spa(data14_closedloop, 30);
 
 % Model training
-nx = 5;
-
-n4sid_data11_closedloop = n4sid(data11_closedloop,nx,'Ts',Ts,n4sidOpt);
-n4sid_data11_closedloop.InputName  = data11_closedloop.InputName;n4sid_data11_closedloop.OutputName = data11_closedloop.OutputName;
-%tfest_data11_closedloop = tfest(data11_closedloop,nx);
-
-n4sid_data12_closedloop = n4sid(data12_closedloop,nx,'Ts',Ts,n4sidOpt);
-n4sid_data12_closedloop.InputName  = data12_closedloop.InputName;n4sid_data12_closedloop.OutputName = data12_closedloop.OutputName;
-
-n4sid_data13_closedloop = n4sid(data13_closedloop,nx,'Ts',Ts,n4sidOpt);
-n4sid_data13_closedloop.InputName  = data13_closedloop.InputName;n4sid_data13_closedloop.OutputName = data13_closedloop.OutputName;
-
+nx = 6;
+n4sid_data11_closedloop = n4sid(data11_closedloop,nx,'Ts',Ts,n4sidOpt); n4sid_data11_closedloop.InputName  = data11_closedloop.InputName;n4sid_data11_closedloop.OutputName = data11_closedloop.OutputName;
+n4sid_data12_closedloop = n4sid(data12_closedloop,nx,'Ts',Ts,n4sidOpt); n4sid_data12_closedloop.InputName  = data12_closedloop.InputName;n4sid_data12_closedloop.OutputName = data12_closedloop.OutputName;
+n4sid_data13_closedloop = n4sid(data13_closedloop,nx,'Ts',Ts,n4sidOpt);n4sid_data13_closedloop.InputName  = data13_closedloop.InputName;n4sid_data13_closedloop.OutputName = data13_closedloop.OutputName;
+n4sid_data14_closedloop = n4sid(data14_closedloop,nx,'Ts',Ts,n4sidOpt);n4sid_data14_closedloop.InputName  = data14_closedloop.InputName;n4sid_data14_closedloop.OutputName = data14_closedloop.OutputName;
 
 %% Figures Open Loop
 fig1 = figure(1);ax1 = axes(fig1); hold(ax1, 'on'); title('Open loop');
@@ -195,7 +229,7 @@ fig1 = figure(1);ax1 = axes(fig1); hold(ax1, 'on'); title('Open loop');
 h = bodeplot(g_data11_openloop   ,opts1, "r*");
 bodeplot(g_data12_openloop   ,opts1, "m*");
 bodeplot(g_data13_openloop , opts1 , "b*");
-showConfidence(h,3); 
+bodeplot(g_data14_openloop , opts1 , "g*");
 
 bodeplot(n4sid_data11_openloop ,opts1, "r--")
 %bodeplot(fpga_n4sid_data11_openloop  ,opts1, "r+--" )
@@ -204,57 +238,57 @@ bodeplot(n4sid_data12_openloop ,opts1, "m--")
 %bodeplot(fpga_n4sid_data12_openloop  ,opts1, "m+--" )
 
 bodeplot(n4sid_data13_openloop ,opts1, "b--")
-%bodeplot(fpga_n4sid_data12_openloop  ,opts1, "m+--" )
+%bodeplot(fpga_n4sid_data13_openloop  ,opts1, "m+--" )
+
+bodeplot(n4sid_data14_openloop ,opts1, "g--")
+%bodeplot(fpga_n4sid_data14_openloop  ,opts1, "m+--" )
 
 legend(); grid on
 
 %% Figures Closed Loop
-% fig2 = figure(2);ax2 = axes(fig2); hold(ax2, 'on'); title('Closed loop');
-% 
-% h = bodeplot(g_data11_closedloop   ,opts1, "r*");
-% bodeplot(g_data12_closedloop   ,opts1, "m*")
-% %showConfidence(h,3);
-% 
+fig2 = figure(2);ax2 = axes(fig2); hold(ax2, 'on'); title('Closed loop');
+h = bodeplot(g_data11_closedloop   ,opts1, "r*");
+bodeplot(g_data12_closedloop   ,opts1, "m*")
+bodeplot(g_data13_closedloop   ,opts1, "b*")
+bodeplot(g_data14_closedloop   ,opts1, "g*")
 % bodeplot(n4sid_data11_closedloop ,opts1)
 % %bodeplot(tfest_data11_closedloop,opts1)
 % bodeplot(G_PIDF_tracking_data11 , opts1 )
 % bodeplot(G_PIDF_true_tune_data11 , opts1)
-% 
 % bodeplot(n4sid_data12_closedloop ,opts1)
-% bodeplot(G_PIDF_tracking_data12 , opts1 )
-% bodeplot(G_PIDF_true_tune_data12 , opts1)
-% 
-% legend(); grid on;
+% % bodeplot(G_PIDF_tracking_data12 , opts1 )
+% % bodeplot(G_PIDF_true_tune_data12 , opts1)
+legend(); grid on;
 
-fig2 = figure(2);ax2 = axes(fig2); hold(ax2, 'on'); title('Closed loop');
+%
+fig11 = figure(11);ax11 = axes(fig11); hold(ax11, 'on'); title('Closed loop');
 h = bodeplot(g_data11_closedloop   ,opts1, "r*");
-%showConfidence(h,3);
-
 bodeplot(n4sid_data11_closedloop ,opts1)
 %bodeplot(tfest_data11_closedloop,opts1)
 %bodeplot(G_PIDF_tracking_data11 , opts1 )
 bodeplot(G_PIDF_true_tune_data11 , opts1)
-
 legend("estimated freq response using spectral analysis","n4sid model","expected CL from identified OL and known controller"); grid on;
 
 %
 fig12 = figure(12);ax12 = axes(fig12); hold(ax12, 'on'); title('Closed loop');
 h=bodeplot(g_data12_closedloop   ,opts1, "m*");
-%showConfidence(h,3);
-
 bodeplot(n4sid_data12_closedloop ,opts1)
 %bodeplot(G_PIDF_tracking_data12 , opts1 )
 bodeplot(G_PIDF_true_tune_data12 , opts1)
-
 legend("estimated freq response using spectral analysis","n4sid model","expected CL from identified OL and known controller"); grid on;
 
 %
 fig13 = figure(13);ax13 = axes(fig13); hold(ax13, 'on'); title('Closed loop');
-h=bodeplot(g_data13_closedloop   ,opts1, "m*");
-%showConfidence(h,3);
-
+h=bodeplot(g_data13_closedloop   ,opts1, "b*");
 bodeplot(n4sid_data13_closedloop ,opts1)
 %bodeplot(G_PIDF_tracking_data13 , opts1 )
 bodeplot(G_PIDF_true_tune_data13 , opts1)
+legend("estimated freq response using spectral analysis","n4sid model","expected CL from identified OL and known controller"); grid on;
 
+%
+fig14 = figure(14);ax14 = axes(fig14); hold(ax14, 'on'); title('Closed loop');
+h=bodeplot(g_data14_closedloop   ,opts1, "b*");
+bodeplot(n4sid_data14_closedloop ,opts1)
+%bodeplot(G_PIDF_tracking_data13 , opts1 )
+bodeplot(G_PIDF_true_tune_data14 , opts1)
 legend("estimated freq response using spectral analysis","n4sid model","expected CL from identified OL and known controller"); grid on;
