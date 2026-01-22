86d301faa4ee3c39533623fa8553db8b6c1a8bb2
c-radicallis <afonso.henrique@live.com.pt>
2026-01-22 11:45:28 +0000

    converted everything to bits

--- ID_and_tuning_in_bits.m
+++ ID_and_tuning_in_bits.m
@@ -24,15 +24,15 @@ np_OL=4;
 input_file_folder ='C:\Users\afons\OneDrive - Universidade de Lisboa\Controlo de Plataforma Sismica\minimesa_data\31-7-2025\tgt and noise drv\';
 file = 'pink_noise_40Hz_T3mm_0.drv'; % load input drv
 LTF_to_TXT_then_load( file , 'InputFolder', input_file_folder , 'OutputFolder', input_file_folder); % load input drv
-x_drv_T_0 = x_drv_T_0*1e3; % convert to mm
+x_drv_T_0 = mm2bits(x_drv_T_0*1e3); % convert to mm
 clear x_drv_L_0  x_drv_V_0
 
 %%  data_P7
 folder_0711 ='C:\Users\afons\OneDrive - Universidade de Lisboa\Controlo de Plataforma Sismica\minimesa_data\7-11-2025\';
 file = 'pink_noise_40Hz_T3mm_0_P7.acq'; % load output acq
 LTF_to_TXT_then_load_wSV( file , folder_0711 , 'OutputFolder', folder_0711);
-x_acq_T = x_acq_T*1e3;
-sv2_acq = bits2mm( -sv2_acq ); %output is inverted because the wiring is fliped
+x_acq_T = mm2bits(x_acq_T*1e3);
+sv2_acq = -sv2_acq; %output is inverted because the wiring is fliped
 Kp=7
 results_P7_pink = twoStageMethod(Kp , fir_np, np_CL , np_OL,  Ts , opts1, sv2_acq, x_drv_T_0, time_drv_0, time_acq, x_acq_T);
 
@@ -40,8 +40,8 @@ results_P7_pink = twoStageMethod(Kp , fir_np, np_CL , np_OL,  Ts , opts1, sv2_ac
 folder_0711 ='C:\Users\afons\OneDrive - Universidade de Lisboa\Controlo de Plataforma Sismica\minimesa_data\7-11-2025\';
 file = 'pink_noise_40Hz_T3mm_0_P10.acq'; % load output acq
 LTF_to_TXT_then_load_wSV( file , folder_0711 , 'OutputFolder', folder_0711);
-x_acq_T = x_acq_T*1e3;
-sv2_acq = bits2mm(-sv2_acq); %output is inverted because the wiring is fliped
+x_acq_T = mm2bits(x_acq_T*1e3);
+sv2_acq = -sv2_acq; %output is inverted because the wiring is fliped
 Kp=10
 results_P10_pink = twoStageMethod(Kp , fir_np, np_CL , np_OL,  Ts , opts1, sv2_acq, x_drv_T_0, time_drv_0, time_acq, x_acq_T);
 
@@ -49,8 +49,8 @@ results_P10_pink = twoStageMethod(Kp , fir_np, np_CL , np_OL,  Ts , opts1, sv2_a
 folder_0711 ='C:\Users\afons\OneDrive - Universidade de Lisboa\Controlo de Plataforma Sismica\minimesa_data\7-11-2025\';
 file = 'pink_noise_40Hz_T3mm_0_P15.acq'; % load output acq
 LTF_to_TXT_then_load_wSV( file , folder_0711 , 'OutputFolder', folder_0711);
-x_acq_T = x_acq_T*1e3;
-sv2_acq = bits2mm(-sv2_acq); %output is inverted because the wiring is fliped
+x_acq_T = mm2bits(x_acq_T*1e3);
+sv2_acq = -sv2_acq; %output is inverted because the wiring is fliped
 Kp=15
 results_P15_pink = twoStageMethod(Kp , fir_np, np_CL , np_OL,  Ts , opts1, sv2_acq, x_drv_T_0, time_drv_0, time_acq, x_acq_T);
 
@@ -58,8 +58,8 @@ results_P15_pink = twoStageMethod(Kp , fir_np, np_CL , np_OL,  Ts , opts1, sv2_a
 folder_1201 ='C:\Users\afons\OneDrive - Universidade de Lisboa\Controlo de Plataforma Sismica\minimesa_data\12-1-2026\pink_noise';
 file = 'wc_10Hz_0.acq'; % load output acq
 LTF_to_TXT_then_load_wSV( file , folder_1201 , 'OutputFolder', folder_1201);
-x_acq_T = x_acq_T*1e3;
-sv2_acq = bits2mm(-sv2_acq); %output is inverted because the wiring is fliped
+x_acq_T = mm2bits(x_acq_T*1e3);
+sv2_acq = -sv2_acq; %output is inverted because the wiring is fliped
 % w_c = 10Hz
 Kp = 6.75;
 Ki = 68;
@@ -72,8 +72,8 @@ results_wc10_pink = twoStageMethod( controller_at_200 , fir_np, np_CL , np_OL,  
 folder_1201 ='C:\Users\afons\OneDrive - Universidade de Lisboa\Controlo de Plataforma Sismica\minimesa_data\12-1-2026\pink_noise';
 file = 'wc_15Hz_0.acq'; % load output acq
 LTF_to_TXT_then_load_wSV( file , folder_1201 , 'OutputFolder', folder_1201);
-x_acq_T = x_acq_T*1e3;
-sv2_acq = bits2mm(-sv2_acq); %output is inverted because the wiring is fliped
+x_acq_T = mm2bits(x_acq_T*1e3);
+sv2_acq = -sv2_acq; %output is inverted because the wiring is fliped
 % w_c = 15Hz
 Kp = 8.75
 Ki = 139
@@ -87,8 +87,8 @@ results_wc15_pink = twoStageMethod(controller_at_200 , fir_np, np_CL , np_OL,  T
 folder_1201 ='C:\Users\afons\OneDrive - Universidade de Lisboa\Controlo de Plataforma Sismica\minimesa_data\12-1-2026\pink_noise';
 file = 'wc_20Hz_0.acq'; % load output acq
 LTF_to_TXT_then_load_wSV( file , folder_1201 , 'OutputFolder', folder_1201);
-x_acq_T = x_acq_T*1e3;
-sv2_acq = bits2mm(-sv2_acq); %output is inverted because the wiring is fliped
+x_acq_T = mm2bits(x_acq_T*1e3);
+sv2_acq = -sv2_acq; %output is inverted because the wiring is fliped
 % w_c = 20Hz
 Kp = 8.562500;
 Ki = 75;
