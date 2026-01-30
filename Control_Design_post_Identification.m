548e42867baab34a568914d405aa7268dd469868
c-radicallis <afonso.henrique@live.com.pt>
2026-01-30 14:36:58 +0000

    modified example to check the effect of the controller on the identification

--- Control_Design_post_Identification.m
+++ Control_Design_post_Identification.m
@@ -84,9 +84,10 @@ Optimal_CL = connect(plant_aug,  controller , integrator, sumblk1, 'x_ref','y_xT
 
 %%
 % %fig9 = figure(9);ax9 = axes(fig9); hold(ax9, 'on');
-% %bodeplot(G_closed,opts1);
-% figure;hold on;
-% bodeplot(G_PIDF_10Hz,opts1);    
+
+figure;hold on;
+bodeplot(G_closed,opts1);
+bodeplot(G_PIDF_10Hz,opts1);    
 % bodeplot(results_wc10_pink.CL_from_OL_est_nonLin,opts1);    
 % bodeplot(results_wc10_pink.CL_from_OL_direct,opts1);    
 % bodeplot(results_wc10_pink.CL,opts1);   
@@ -103,64 +104,64 @@ Optimal_CL = connect(plant_aug,  controller , integrator, sumblk1, 'x_ref','y_xT
 % bodeplot(results_wc20_pink.CL,opts1);  
 
 
-%% Finding Response Spectre of Ground
-f_i=0.1; %freq inicial
-f_n=40;  %freq final
-n_points = 1e2;
-f_vector = logspace( log10(f_i) , log10(f_n) , n_points);
-
-[picos_ddx_ground , picos_x_ground] = ResponseSpectrum( t_vector , x_ref , ddx_ref, f_vector , 1);
-x_cl = lsim(d2d(G_closed,Ts) ,  x_ref , t_vector,'zoh');
-ddx_cl = secondDerivativeTime(x_cl , Ts);
-[picos_ddx_cl , picos_x_cl] = ResponseSpectrum( t_vector , x_cl , ddx_cl, f_vector , 1);
-
-x_PIDF_5Hz = lsim(d2d(G_PIDF_20Hz,Ts) ,  x_ref , t_vector,'zoh');
-ddx_PIDF_5Hz = secondDerivativeTime(x_PIDF_5Hz , Ts);
-[picos_ddx_PIDF_5Hz , picos_x_PIDF_5Hz] = ResponseSpectrum( t_vector , x_PIDF_5Hz , ddx_PIDF_5Hz, f_vector , 1);
-
-x_PIDF_10Hz = lsim(d2d(G_PIDF_10Hz,Ts) ,  x_ref , t_vector,'zoh');
-ddx_PIDF_10Hz = secondDerivativeTime(x_PIDF_10Hz , Ts);
-[picos_ddx_PIDF_10Hz , picos_x_PIDF_10Hz] = ResponseSpectrum( t_vector , x_PIDF_10Hz , ddx_PIDF_10Hz, f_vector , 1);
-
-x_PIDF_15Hz = lsim(d2d(G_PIDF_15Hz,Ts) ,  x_ref , t_vector,'zoh');
-ddx_PIDF_15Hz = secondDerivativeTime(x_PIDF_15Hz , Ts);
-[picos_ddx_PIDF_15Hz , picos_x_PIDF_15Hz] = ResponseSpectrum( t_vector , x_PIDF_15Hz , ddx_PIDF_15Hz, f_vector , 1);
-
-x_Optimal = lsim(d2d(Optimal_CL,Ts) ,  x_ref , t_vector,'zoh');
-ddx_Optimal = secondDerivativeTime(x_Optimal , Ts);
-[picos_ddx_Optimal , picos_x_Optimal] = ResponseSpectrum( t_vector , x_Optimal , ddx_Optimal, f_vector , 1);
-
-baseFolder = 'C:\Users\afons\OneDrive - Universidade de Lisboa\Controlo de Plataforma Sismica\minimesa_data\figures\';   % Base folder where you want to create the timestamped subfolder
-ts = datestr(now, 'yyyymmdd_HHMM');  % Create a timestamp string, e.g. '20250709_1530'
-timeDir = fullfile(baseFolder, ts);  % Build the full path to the new folder
-if ~exist(timeDir, 'dir')% Create it if it doesn't already exist
-    mkdir(timeDir)
-end
-
-fig8 = figure(8);subplot(121); grid on;xlabel('Frequency (Hz)');ylabel('Acceleration (m/s^2)');title('Acceleration Response Spectra - Fault Normal');xlim([1 20]);%ylim([0 ceil(max(picos_ddx_T_tuned(1:385,1))) ])
-subplot(122);grid on;xlabel('Frequency (Hz)');ylabel('Displacement (m)');title('Displacement Response Spectra - Fault Normal');xlim([0.1 5]);
-color1 = 'blue';color2 = 'red' ;color3 = '#EDB120'; color4 = 'black';% Define colors for lines 1/3 and 2/4
-
-figure(fig8); subplot(121); grid on; legend(); hold on;
-plot(f_vector, picos_ddx_ground,'-', 'LineWidth' , 2, 'Color', color1, 'DisplayName', 'Target');% - Normal
-plot(f_vector, picos_ddx_cl,'--', 'LineWidth' , 2, 'Color', color2, 'DisplayName',sprintf( 'Standard -  MSE= %.2e',      mean((picos_ddx_ground-picos_ddx_cl).^2 )));% - Normal
-plot(f_vector, picos_ddx_PIDF_10Hz,'--', 'LineWidth' , 2 , 'Color', color3, 'DisplayName',sprintf( 'PIDF 10Hz - MSE= %.2e',   mean((picos_ddx_ground-picos_ddx_PIDF_10Hz).^2 )));
-plot(f_vector, picos_ddx_PIDF_15Hz ,'-', 'LineWidth' , 2, 'Color', color4, 'DisplayName',sprintf( 'PIDF 15Hz - MSE= %.2e', mean((picos_ddx_ground-picos_ddx_PIDF_15Hz).^2 )));
-plot(f_vector, picos_ddx_Optimal ,'-', 'LineWidth' , 2, 'DisplayName',sprintf( 'Optimal Control -  MSE= %.2e', mean((picos_ddx_ground-picos_ddx_Optimal).^2 )));
-
-subplot(122); grid on;legend();hold on;
-plot(f_vector, picos_x_ground,'-', 'LineWidth' , 2, 'Color', color1, 'DisplayName', 'Target ');%- Normal
-plot(f_vector, picos_x_cl,'--', 'LineWidth' , 2, 'Color', color2, 'DisplayName',sprintf( 'Standard - MSE= %.2e',     mean((picos_x_ground-picos_x_cl).^2 )));%- Normal
-plot(f_vector, picos_x_PIDF_10Hz,'--', 'LineWidth' , 2, 'Color', color3, 'DisplayName',sprintf( 'PIDF 10Hz - MSE= %.2e',  mean((picos_x_ground-picos_x_PIDF_10Hz).^2 )));
-plot(f_vector, picos_x_PIDF_15Hz, '-', 'LineWidth' , 2, 'Color', color4, 'DisplayName',sprintf( 'PIDF 15Hz - MSE= %.2e', mean((picos_x_ground-picos_x_PIDF_15Hz).^2 )));
-plot(f_vector, picos_x_Optimal, '-', 'LineWidth' , 2,  'DisplayName',sprintf( 'Optimal Control - MSE= %.2e', mean((picos_x_ground-picos_x_Optimal).^2 )));
-
-set(fig8, 'WindowState', 'maximized');
-exportgraphics(fig8,fullfile(timeDir,'Response_Spectra_N.png'),'Resolution', 300,'BackgroundColor', 'white','ContentType', 'image');
-
-%%
-figure; hold on; grid on;
-plot(t_vector, x_ref);
-plot(t_vector, x_cl);
-plot(t_vector, x_PIDF_15Hz);
-plot(t_vector , x_Optimal)
+% %% Finding Response Spectre of Ground
+% f_i=0.1; %freq inicial
+% f_n=40;  %freq final
+% n_points = 1e2;
+% f_vector = logspace( log10(f_i) , log10(f_n) , n_points);
+% 
+% [picos_ddx_ground , picos_x_ground] = ResponseSpectrum( t_vector , x_ref , ddx_ref, f_vector , 1);
+% x_cl = lsim(d2d(G_closed,Ts) ,  x_ref , t_vector,'zoh');
+% ddx_cl = secondDerivativeTime(x_cl , Ts);
+% [picos_ddx_cl , picos_x_cl] = ResponseSpectrum( t_vector , x_cl , ddx_cl, f_vector , 1);
+% 
+% x_PIDF_5Hz = lsim(d2d(G_PIDF_20Hz,Ts) ,  x_ref , t_vector,'zoh');
+% ddx_PIDF_5Hz = secondDerivativeTime(x_PIDF_5Hz , Ts);
+% [picos_ddx_PIDF_5Hz , picos_x_PIDF_5Hz] = ResponseSpectrum( t_vector , x_PIDF_5Hz , ddx_PIDF_5Hz, f_vector , 1);
+% 
+% x_PIDF_10Hz = lsim(d2d(G_PIDF_10Hz,Ts) ,  x_ref , t_vector,'zoh');
+% ddx_PIDF_10Hz = secondDerivativeTime(x_PIDF_10Hz , Ts);
+% [picos_ddx_PIDF_10Hz , picos_x_PIDF_10Hz] = ResponseSpectrum( t_vector , x_PIDF_10Hz , ddx_PIDF_10Hz, f_vector , 1);
+% 
+% x_PIDF_15Hz = lsim(d2d(G_PIDF_15Hz,Ts) ,  x_ref , t_vector,'zoh');
+% ddx_PIDF_15Hz = secondDerivativeTime(x_PIDF_15Hz , Ts);
+% [picos_ddx_PIDF_15Hz , picos_x_PIDF_15Hz] = ResponseSpectrum( t_vector , x_PIDF_15Hz , ddx_PIDF_15Hz, f_vector , 1);
+% 
+% x_Optimal = lsim(d2d(Optimal_CL,Ts) ,  x_ref , t_vector,'zoh');
+% ddx_Optimal = secondDerivativeTime(x_Optimal , Ts);
+% [picos_ddx_Optimal , picos_x_Optimal] = ResponseSpectrum( t_vector , x_Optimal , ddx_Optimal, f_vector , 1);
+% 
+% baseFolder = 'C:\Users\afons\OneDrive - Universidade de Lisboa\Controlo de Plataforma Sismica\minimesa_data\figures\';   % Base folder where you want to create the timestamped subfolder
+% ts = datestr(now, 'yyyymmdd_HHMM');  % Create a timestamp string, e.g. '20250709_1530'
+% timeDir = fullfile(baseFolder, ts);  % Build the full path to the new folder
+% if ~exist(timeDir, 'dir')% Create it if it doesn't already exist
+%     mkdir(timeDir)
+% end
+% 
+% fig8 = figure(8);subplot(121); grid on;xlabel('Frequency (Hz)');ylabel('Acceleration (m/s^2)');title('Acceleration Response Spectra - Fault Normal');xlim([1 20]);%ylim([0 ceil(max(picos_ddx_T_tuned(1:385,1))) ])
+% subplot(122);grid on;xlabel('Frequency (Hz)');ylabel('Displacement (m)');title('Displacement Response Spectra - Fault Normal');xlim([0.1 5]);
+% color1 = 'blue';color2 = 'red' ;color3 = '#EDB120'; color4 = 'black';% Define colors for lines 1/3 and 2/4
+% 
+% figure(fig8); subplot(121); grid on; legend(); hold on;
+% plot(f_vector, picos_ddx_ground,'-', 'LineWidth' , 2, 'Color', color1, 'DisplayName', 'Target');% - Normal
+% plot(f_vector, picos_ddx_cl,'--', 'LineWidth' , 2, 'Color', color2, 'DisplayName',sprintf( 'Standard -  MSE= %.2e',      mean((picos_ddx_ground-picos_ddx_cl).^2 )));% - Normal
+% plot(f_vector, picos_ddx_PIDF_10Hz,'--', 'LineWidth' , 2 , 'Color', color3, 'DisplayName',sprintf( 'PIDF 10Hz - MSE= %.2e',   mean((picos_ddx_ground-picos_ddx_PIDF_10Hz).^2 )));
+% plot(f_vector, picos_ddx_PIDF_15Hz ,'-', 'LineWidth' , 2, 'Color', color4, 'DisplayName',sprintf( 'PIDF 15Hz - MSE= %.2e', mean((picos_ddx_ground-picos_ddx_PIDF_15Hz).^2 )));
+% plot(f_vector, picos_ddx_Optimal ,'-', 'LineWidth' , 2, 'DisplayName',sprintf( 'Optimal Control -  MSE= %.2e', mean((picos_ddx_ground-picos_ddx_Optimal).^2 )));
+% 
+% subplot(122); grid on;legend();hold on;
+% plot(f_vector, picos_x_ground,'-', 'LineWidth' , 2, 'Color', color1, 'DisplayName', 'Target ');%- Normal
+% plot(f_vector, picos_x_cl,'--', 'LineWidth' , 2, 'Color', color2, 'DisplayName',sprintf( 'Standard - MSE= %.2e',     mean((picos_x_ground-picos_x_cl).^2 )));%- Normal
+% plot(f_vector, picos_x_PIDF_10Hz,'--', 'LineWidth' , 2, 'Color', color3, 'DisplayName',sprintf( 'PIDF 10Hz - MSE= %.2e',  mean((picos_x_ground-picos_x_PIDF_10Hz).^2 )));
+% plot(f_vector, picos_x_PIDF_15Hz, '-', 'LineWidth' , 2, 'Color', color4, 'DisplayName',sprintf( 'PIDF 15Hz - MSE= %.2e', mean((picos_x_ground-picos_x_PIDF_15Hz).^2 )));
+% plot(f_vector, picos_x_Optimal, '-', 'LineWidth' , 2,  'DisplayName',sprintf( 'Optimal Control - MSE= %.2e', mean((picos_x_ground-picos_x_Optimal).^2 )));
+% 
+% set(fig8, 'WindowState', 'maximized');
+% exportgraphics(fig8,fullfile(timeDir,'Response_Spectra_N.png'),'Resolution', 300,'BackgroundColor', 'white','ContentType', 'image');
+% 
+% %%
+% figure; hold on; grid on;
+% plot(t_vector, x_ref);
+% plot(t_vector, x_cl);
+% plot(t_vector, x_PIDF_15Hz);
+% plot(t_vector , x_Optimal)
--- example_10_7_5_two_stage_method.m
+++ example_10_7_5_two_stage_method.m
@@ -1,7 +1,7 @@
 % Example 10.7.5 - two stage closed loop identification method 
 clear;clc; setappdata(0, 'AutoStagger_LRDown_Last', []);   % ensure first figure starts at top-left
 set(0, 'DefaultFigureCreateFcn', @autoStagger_LRDown_relSize);
-%close all;
+close all;
 
 % bode plot options
 opts1=bodeoptions('cstprefs');opts1.MagLowerLimMode = 'manual'; opts1.MagLowerLim=-25;opts1.XLim={[0.05 4]};opts1.PhaseWrapping="on";%opts1.PhaseWrappingBranch=-360;
@@ -11,16 +11,18 @@ opts2=bodeoptions('cstprefs');opts2.MagLowerLimMode = 'manual';opts2.MagLowerLim
 z=tf('z');
 Ts=1;
 t = [0:Ts:2048-1]';
-e = 1*wgn(2048 , 1 , 0.1 );
+e = 0.1*wgn(2048 , 1 , 0.1 );
 r1 = 0*wgn(2048 , 1 , 0.1 );
 r2 = 1*wgn(2048 , 1 , 0.1 );
 
-C=z^-1 - 0.8*z^-2
-np=2
-nz=2
-
+np=2;
+nz=2;
 G0 = 1/(1-1.6*z^-1+0.89*z^-2)
 H0 = (1 - 1.56*z^-1 + 1.045*z^-2 -0.3338*z^-3)/(1 - 2.35*z^-1 + 2.09*z^-2 -0.6675*z^-3)
+
+for i = [0.01, 0.1 , 0.5 , 1 ,1.5  ] %1.9  %2-stage stops working when S0 is close to instable
+
+C=i*(z^-1 - 0.8*z^-2)
 S0 = 1/(1+G0*C)
 
 r = r1 + lsim( C , r2 , t );
@@ -34,12 +36,12 @@ u_r =  lsim(S0 , r ,t);
 % legend;
 
 %% step1
-S_est = polyest(r,u ,  [[0 15 0 0 0] 0],'Ts',Ts)%tfest(r,u,9)%armax(r,u , [9*[1 1 1] 1],opt)%oe(r ,u , [ 8 8 1 ] )%
+S_est = polyest(r,u ,  [[0 15 0 0 0] 0],'Ts',Ts);%tfest(r,u,9)%armax(r,u , [9*[1 1 1] 1],opt)%oe(r ,u , [ 8 8 1 ] )%
 
-figure; hold on;
-bodeplot(S0,'r', opts1);
-bodeplot(S_est,'g', opts1);
-legend; grid on;
+% figure; hold on;
+% bodeplot(S0,'r', opts1);
+% bodeplot(S_est,'g', opts1);
+% legend; grid on;
 
 %% step 2
 u_r_est = lsim(S_est , r , t);
@@ -63,7 +65,7 @@ legend; grid on;
 %% RESID
 %opt_resid = residOptions('MaxLag',20);
 figure;
-resid(iddata(y,u,Ts),G0,G_direct,G_est)%,opt_resid);%,OL_direct,OL_indirect
+resid(iddata(y,u,Ts),G0,'r',G_direct,'b-.',G_est,'g--')%,opt_resid);%,OL_direct,OL_indirect
 legend; grid on;
 
-
+end
