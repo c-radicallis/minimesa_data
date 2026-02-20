82fc2dbd63690655a6c08783361a3d4998d38fa8
c-radicallis <afonso.henrique@live.com.pt>
2026-02-20 15:00:33 +0000

    commit

--- plot_results.m
+++ plot_results.m
@@ -4,7 +4,6 @@ addpath 'C:\Users\afons\OneDrive - Universidade de Lisboa\Controlo de Plataforma
 func_folder  =  'C:\Users\afons\OneDrive - Universidade de Lisboa\Controlo de Plataforma Sismica\uniaxial_table_model\Adapting_Driver_Signal\';
 addpath(func_folder);
 Ts = 0.005;
-Ts_fpga= 1/5000;
 
 % bode plot options
 opts1=bodeoptions('cstprefs');opts1.FreqUnits = 'Hz';opts1.XLim={[1 100]};opts1.PhaseWrapping="on";opts1.PhaseWrappingBranch=-360;%opts1.Ylim={[-40 10]};
@@ -50,9 +49,6 @@ file = 'TolmezzoReducedScale_4.acq'; % load output acq
 LTF_to_TXT_then_load_wSV( file , folder_1201_tolmezzo_drv_acq );
 ddx_acq_T_comp = secondDerivativeTime(x_acq_T , Ts);
 
-%% Create Figures - Transversal
-close all;
-
 % Response Spectra settings
 f_i=0.1; %freq inicial
 f_n=30;  %freq final
@@ -69,13 +65,6 @@ f_vector = logspace( log10(f_i) , log10(f_n) , n_points);
 
 % Adapted driver response spectra
 [picos_ddx_T_acq_4  , picos_x_T_acq_4 ] = ResponseSpectrum( time_vector , x_acq_T, ddx_acq_T_comp, f_vector , 1);
-
-baseFolder = folder_1201;   % Base folder where you want to create the timestamped subfolder
-ts = datestr(now, 'yyyymmdd_HHMM');  % Create a timestamp string, e.g. '20250709_1530'
-timeDir = fullfile(baseFolder, ts);  % Build the full path to the new folder
-if ~exist(timeDir, 'dir')% Create it if it doesn't already exist
-    mkdir(timeDir)
-end
 
 fig8 = figure(8);subplot(121); grid on;xlabel('Frequency (Hz)');ylabel('Acceleration (m/s^2)');title('Acceleration Response Spectra - Fault Normal');xlim([1 20]);%ylim([0 ceil(max(picos_ddx_T_tuned_20hz(1:385,1))) ])
 subplot(122);grid on;xlabel('Frequency (Hz)');ylabel('Displacement (m)');title('Displacement Response Spectra - Fault Normal');xlim([0.1 5]);
@@ -127,12 +116,16 @@ ddx_acq_T_20hz_comp = secondDerivativeTime(x_acq_T , Ts); % No acceleration data
 
 %%Computing Response spectra of Adapted
 folder_1201_laquila_drv_acq ='C:\Users\afons\OneDrive - Universidade de Lisboa\Controlo de Plataforma Sismica\minimesa_data\12-1-2026\laquila_drv&acq\';
-file = 'laquilaReducedScale_4.acq'; % load output acq
-LTF_to_TXT_then_load_wSV( file , folder_1201_tolmezzo_drv_acq );
-ddx_acq_T_comp = secondDerivativeTime(x_acq_T , Ts);
 
-%% Create Figures 
-close all;
+file = 'laquilaReducedScale_2.acq'; % load output acq
+LTF_to_TXT_then_load_wSV( file , folder_1201_laquila_drv_acq );
+x_acq_T_2=x_acq_T;
+ddx_acq_T_2_comp = secondDerivativeTime(x_acq_T_2 , Ts);
+
+file = 'laquilaReducedScale_4.acq'; % load output acq
+LTF_to_TXT_then_load_wSV( file , folder_1201_laquila_drv_acq );
+x_acq_T_4=x_acq_T;
+ddx_acq_T_4_comp = secondDerivativeTime(x_acq_T_4 , Ts);
 
 % Response Spectra settings
 f_i=0.1; %freq inicial
@@ -149,14 +142,8 @@ f_vector = logspace( log10(f_i) , log10(f_n) , n_points);
 [picos_ddx_T_tuned_20hz, picos_x_T_tuned_20hz] = ResponseSpectrum( time_vector , x_acq_T_20hz , ddx_acq_T_20hz_comp, f_vector , 1);
 
 % Adapted driver response spectra
-[picos_ddx_T_acq_4  , picos_x_T_acq_4 ] = ResponseSpectrum( time_vector , x_acq_T, ddx_acq_T_comp, f_vector , 1);
-
-baseFolder = folder_1201;   % Base folder where you want to create the timestamped subfolder
-ts = datestr(now, 'yyyymmdd_HHMM');  % Create a timestamp string, e.g. '20250709_1530'
-timeDir = fullfile(baseFolder, ts);  % Build the full path to the new folder
-if ~exist(timeDir, 'dir')% Create it if it doesn't already exist
-    mkdir(timeDir)
-end
+[picos_ddx_T_acq_2  , picos_x_T_acq_2 ] = ResponseSpectrum( time_vector , x_acq_T_2, ddx_acq_T_2_comp, f_vector , 1);
+[picos_ddx_T_acq_4  , picos_x_T_acq_4 ] = ResponseSpectrum( time_vector , x_acq_T_4, ddx_acq_T_4_comp, f_vector , 1);
 
 fig9 = figure(9);subplot(121); grid on;xlabel('Frequency (Hz)');ylabel('Acceleration (m/s^2)');title('Acceleration Response Spectra - Fault Normal');xlim([1 20]);%ylim([0 ceil(max(picos_ddx_T_tuned_20hz(1:395,1))) ])
 subplot(122);grid on;xlabel('Frequency (Hz)');ylabel('Displacement (m)');title('Displacement Response Spectra - Fault Normal');xlim([0.1 5]);
@@ -176,11 +163,17 @@ plot(f_vector, picos_x_tgt_T,'-', 'LineWidth' , 2, 'Color', color1, 'DisplayName
 plot(f_vector, picos_x_T_tuned_10hz,'--', 'LineWidth' , 2, 'DisplayName',sprintf( 'PIDF ω_c=10Hz - MSE= %.2e',     mean((picos_x_tgt_T-picos_x_T_tuned_10hz).^2 )));%- Normal
 plot(f_vector, picos_x_T_tuned_15hz,'--', 'LineWidth' , 2, 'DisplayName',sprintf( 'PIDF ω_c=15Hz - MSE= %.2e',     mean((picos_x_tgt_T-picos_x_T_tuned_15hz).^2 )));%- Normal
 plot(f_vector, picos_x_T_tuned_20hz,'--', 'LineWidth' , 2, 'DisplayName',sprintf( 'PIDF ω_c=20Hz - MSE= %.2e',     mean((picos_x_tgt_T-picos_x_T_tuned_20hz).^2 )));%- Normal
-plot(f_vector, picos_x_T_acq_4, '-', 'LineWidth' , 2, 'Color', color4, 'DisplayName',sprintf( 'Adapted driver 4 - MSE= %.2e', mean((picos_x_tgt_T-picos_x_T_acq_4).^2 )));
-
-
+plot(f_vector, picos_x_T_acq_2, '-', 'LineWidth' , 2, 'DisplayName',sprintf( 'Adapted driver 2 - MSE= %.2e', mean((picos_x_tgt_T-picos_x_T_acq_2).^2 )));
+plot(f_vector, picos_x_T_acq_4, '-', 'LineWidth' , 2, 'DisplayName',sprintf( 'Adapted driver 4 - MSE= %.2e', mean((picos_x_tgt_T-picos_x_T_acq_4).^2 )));
 
 %%
+baseFolder = folder_1201;   % Base folder where you want to create the timestamped subfolder
+ts = datestr(now, 'yyyymmdd_HHMM');  % Create a timestamp string, e.g. '20250709_1530'
+timeDir = fullfile(baseFolder, ts);  % Build the full path to the new folder
+if ~exist(timeDir, 'dir')% Create it if it doesn't already exist
+    mkdir(timeDir)
+end
+
 set(fig8, 'WindowState', 'maximized');
 exportgraphics(fig8,fullfile(timeDir,'Response_Spectra_N.png'),'Resolution', 300,'BackgroundColor', 'white','ContentType', 'image');
 
